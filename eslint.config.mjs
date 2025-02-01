7d98f11781fc448e59151c746200c3b4743b4190
Aakash Rajbhar <akash.kumarajbhar@gmail.com>
2025-02-01 15:52:31 +0530

    initial commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,14 @@
-import { Linter } from 'eslint';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
 
-const eslintConfig = {
-  extends: ['next/core-web-vitals'],
-};
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [...compat.extends('next/core-web-vitals')];
 
 export default eslintConfig;
